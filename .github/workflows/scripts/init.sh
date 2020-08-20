9babd88172ce61878274c2d403df6ae060e2447f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-20 15:28:49 +0200

    Updated init with bot usage + GPG signing

--- .github/workflows/scripts/init.sh
+++ .github/workflows/scripts/init.sh
@@ -5,18 +5,7 @@ npm i kaskadi-cli
 ./node_modules/.bin/kaskadi init layer
 npm rm kaskadi-cli
 # Commit files
-if [ "$GITHUB_ACTOR" == "alexlemaire" ]
-then
-  USERNAME="Alexis Lemaire"
-  EMAIL="a.lemaire@klimapartner.de"
-elif [ "$GITHUB_ACTOR" == "Holger-Will" ]
-then
-  USERNAME="Holger Will"
-  EMAIL="h.will@klimapartner.de"
-fi
-git config --global user.name "$USERNAME"
-git config --global user.email "$EMAIL"
 git rm .github/workflows/init.yml
 git rm .github/workflows/scripts/init.sh
-git commit -am "Initialized repository with correct naming"
+git commit -S -am "Initialized repository with correct naming"
 git push
